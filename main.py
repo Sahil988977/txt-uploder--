aa786def51f7cbc97fb2626332616a8fcc7ed50c
Sahil988977 <103986785+Sahil988977@users.noreply.github.com>
2026-05-09 19:52:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ async def show_random_emojis(message):
     return emoji_message
     
 # Define the owner's user ID
-OWNER_ID = 6414266397 # Replace with the actual owner's user ID
+OWNER_ID = 6478225349 # Replace with the actual owner's user ID
 
 # List of sudo users (initially empty or pre-populated)
 SUDO_USERS = [6414266397,6727160308]
